1c725dc1af6bb0d1ea4b4fc4780afbd25787f9f4
Vishwas Prabhu <vishpra@Vishwass-MacBook-Air.local>
2024-10-05 21:06:25 -0700

    Update model

--- app/model/model.py
+++ app/model/model.py
@@ -1,15 +1,17 @@
 import pickle
 import re
+import mlflow
 from pathlib import Path
 
 __version__ = "0.1.0"
 
 BASE_DIR = Path(__file__).resolve(strict=True).parent
+MLFLOW_RUN_ID = '1e9fb8e091714130b07ac2b9644fa17b'
 
-
-with open(f"{BASE_DIR}/trained_pipeline-{__version__}.pkl", "rb") as f:
-    model = pickle.load(f)
-
+# Load the model from MLflow
+# mlflow_model_path = f'runs:/{MLFLOW_RUN_ID}/language_detection_model'
+mlflow_model_path = f'{BASE_DIR}/mlruns/{MLFLOW_RUN_ID}/artifacts/language_detection_model'  
+model = mlflow.pyfunc.load_model(mlflow_model_path)
 
 classes = [
     "Arabic",
@@ -37,4 +39,4 @@ def predict_pipeline(text):
     text = re.sub(r"[[]]", " ", text)
     text = text.lower()
     pred = model.predict([text])
-    return classes[pred[0]]
+    return pred[0]
